152b2b5e39faf478d60053fd37637787364dfae6
Piero10Barca <dieguitominino25@gmail.com>
2025-04-06 06:53:33 -0500

    build listo para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Simulador-Credito-Piero-Motors/',
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
