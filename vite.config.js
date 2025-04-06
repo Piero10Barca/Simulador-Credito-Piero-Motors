f7eaa41bac9672afffd44655ffb29a19fd9401b1
Piero10Barca <dieguitominino25@gmail.com>
2025-04-06 06:02:10 -0500

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/Simulador-Credito-Piero-Motors/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,3 +11,4 @@ export default defineConfig({
     },
   },
 });
+
